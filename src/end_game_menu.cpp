3d6fba8bd622c8a3ba79aae7453ecd362a0b09a7
axel senecal <senecal.axel89@gmail.com>
2023-12-11 10:56:30 +0100

    fix endgame menu ui

--- src/end_game_menu.cpp
+++ src/end_game_menu.cpp
@@ -19,9 +19,9 @@ void EndGameMenu::render(SDL_Renderer *renderer, int score)
     std::string scoreString = "Your score is : " + std::to_string(score);
 
     // Render text
-    utils::RenderEndText(font, renderer, "Thanks for playing Town Farming", WINDOW_WIDTH/2 - 220, WINDOW_HEIGHT/2 - 380);
+    utils::RenderEndText(font, renderer, "Thanks for playing Town Farming", WINDOW_WIDTH/2 - 220, WINDOW_HEIGHT/14);
     utils::RenderEndText(font, renderer, scoreString.c_str(), WINDOW_WIDTH/2 - 120, WINDOW_HEIGHT/2 - 260);
-    utils::RenderEndText(font, renderer, "Press ESC to quit", WINDOW_WIDTH/2 - 120, WINDOW_HEIGHT/2 + 220);
+    utils::RenderEndText(font, renderer, "Press ESC to quit", WINDOW_WIDTH/2 - 120, WINDOW_HEIGHT - 180);
 
     // Render image
     if (score >= 500)
